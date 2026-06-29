ba5d3cb64d6f3d5fa309c274145d9e73019f238a
Fergus Cooper <3770306+fcooper8472@users.noreply.github.com>
2026-06-29 23:33:18 +0100

    Fix slide navigation by using relative base path

--- build_all.sh
+++ build_all.sh
@@ -1,15 +1,13 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-repo_root="${1:-}"
-
 pushd "$(dirname "$0")" > /dev/null
 
 rm -rf build dist
 mkdir -p build dist
 
 for dir in presentations/*/; do
-    ./build.sh "$dir" "$repo_root"
+    ./build.sh "$dir"
 done
 
 node ./scripts/build-index.mjs
